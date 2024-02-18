2cccae4845c20ab3029f7827dcbfd6e2ba979dcf
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-02-18 18:14:59 +0100

    update header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,16 +5,16 @@ import Image from "next/image"
 import Link from "next/link"
 
 const Home = async ({ searchParams }: SearchParamProps) => {
-  const page = Number(searchParams?.page) || 1;
-  const searchQuery = (searchParams?.query as string) || '';
+  const page = Number(searchParams?.page) || 1
+  const searchQuery = (searchParams?.query as string) || ""
 
-  const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery })
 
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with Imaginify
+          Unleash Your Creative Vision with Imagify
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
@@ -33,7 +33,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="sm:mt-12">
-        <Collection 
+        <Collection
           hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
